5bb265b2b80d70efc4cf68e22cde9d1659fb363d
brandolern <46857695+brandolern@users.noreply.github.com>
2019-03-28 20:30:56 -0600

    api call working

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -60,15 +60,18 @@ $(document).ready(function () {
     });
 
     $(".card").on("click", function () {
-        var queryURL = "";
         var cuisine = ($(this).attr("id"));
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?start=0&count=10&lat=" + lat + "&lon=" + long + "&radius=5000&cuisines=" + cuisine + "&sort=real_distance&order=asc&apikey=0ce7b31696dc922375b5bd5b125d26af";
+
         console.log(cuisine);
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            var name = response.restaurants[0].restaurant.name;
+            console.log(name);
+            console.log(response)
         });
 
 
