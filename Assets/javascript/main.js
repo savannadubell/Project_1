7153db64204be5b8f5ddbf11f2c27f4de8778eb5
C Morawski <morawskiclaire@gmail.com>
2019-03-28 20:56:44 -0600

    Refresh Page

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //var feedMe = $(".feed-me");
 
     // Preloader dissapears after 2 seconds
     setTimeout(function () {
@@ -58,20 +59,78 @@ $(document).ready(function () {
         console.log(zip);
         console.log(state);
     });
-
     $(".card").on("click", function () {
-        var queryURL = "";
         var cuisine = ($(this).attr("id"));
-        console.log(cuisine);
-
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=1213&entity_type=city&cuisines=" + cuisine;
+        // //%20%2B%20random
+        // var cuisine = ($(this).attr("id"));
         $.ajax({
             url: queryURL,
-            method: "GET"
+            method: "GET",
+            headers: {
+                "user-key": "c2f8ca36527489e90ec9ce18254ed96e",
+            }
         }).then(function (response) {
-            console.log(response);
-        });
+            console.log(response)
+            $('.cusine-cards').empty()
+            //display restaurant that was chosen
+            //display buttons of "go here" and "choose a different option for me"
+
+            var startDiv = $('<div>');
+            startDiv.addClass("card");
+
+            var img = $('<img>');
+            img.addClass("card-img-top");
+            img.attr("src", response.restaurants[0].restaurant.featured_image);
+
+            var divTwo = $('<div>');
+            divTwo.addClass("card-body");
+
+            var pClass = $('<p>');
+            pClass.addClass("card-text");
+            pClass.text(response.restaurants[0].restaurant.name);
+
+            //connecting all elements together
+            startDiv.append(img).append(divTwo).append(pClass);
+
+            //print to page
+            $('.cusine-cards').append(startDiv);
 
 
-    })
+            /*
+            
+                    <div class="card" id="italian" style="width: 18rem;">
+                        <img class="card-img-top" src="https://cdn.cnn.com/cnnnext/dam/assets/190107114340-pizza-slice.jpg"
+                            alt="italian-food">
+                        <div class="card-body">
+                            <p class="card-text">ITALIAN FOOD</p>
+                        </div>
+                    </div>
+            
+            */
 
+
+
+
+
+
+
+
+
+
+        }).fail(function (jqXHR, textStatus) {
+            console.log(JSON.stringify(jqXHR))
+        })
+
+
+    });
 });
+
+//empty page after user chooses cuisine type
+$('.cusine-cards').empty()
+
+//displaying on-click functions to page
+
+
+
+//append
