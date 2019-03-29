1ebaa5a6cdfdbb8999aaa7966ae6264c8fc0ace5
Brandon <46857695+brandolern@users.noreply.github.com>
2019-03-28 20:43:02 -0600

    Update main.js

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -5,26 +5,6 @@ $(document).ready(function () {
         $('body').addClass('loaded');
     }, 2000);
 });
-
-    $(".card").on("click", function () {
-        var cuisine = ($(this).attr("id"));
-        var queryURL = "https://developers.zomato.com/api/v2.1/search?start=0&count=10&lat=" + lat + "&lon=" + long + "&radius=5000&cuisines=" + cuisine + "&sort=real_distance&order=asc&apikey=0ce7b31696dc922375b5bd5b125d26af";
-
-        console.log(cuisine);
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            var name = response.restaurants[0].restaurant.name;
-            console.log(name);
-            console.log(response)
-        });
-
-
-    })
-
-});
     //Location variables
     var lat
     var long
